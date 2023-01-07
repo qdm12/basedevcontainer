0660c2da402a522a54e50019492b3017c9b31912
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-01-07 13:06:22 +0100

    fix(ssh): idempotent ssh script - Leave ~/.ssh bind mount - Leave ~/.ssh if /mnt/ssh has the same file names - Leave ~/.ssh if /tmp/.ssh has the same file names - Otherwise copies or symlinks to ~/.ssh

--- .ssh.sh
+++ .ssh.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 if [ -d ~/.ssh ]; then
-  # If the ~/.ssh directory exists, it's either
-  # already populated by some custom configuration,
-  # or it has been bind mounted with an older docker-compose.yml
-  # for Linux and OSX, so we should leave it as is.
-  echo "~/.ssh directory already exists, exiting"
-  return 0
+  if echo "$(mountpoint ~/.ssh)" | grep -q "is a mountpoint"; then
+    # ~/.ssh is a bind mount from the host
+    return 0;
+  fi
+  if [ -d /mnt/ssh ] && [ -z "$(comm -3 <(/bin/ls -a /mnt/ssh) <(/bin/ls -a ~/.ssh))" ]; then
+    # /mnt/ssh and ~/.ssh are the same in terms of file names.
+    return 0;
+  fi
+  if [ -d /tmp/.ssh ] && [ -z "$(comm -3 <(/bin/ls -a /tmp/.ssh) <(/bin/ls -a ~/.ssh))" ]; then
+    # Retro-compatibility: /tmp/.ssh and ~/.ssh are the same in terms of file names.
+    return 0;
+  fi
 fi
 
 if [ -d /tmp/.ssh ]; then
   # Retro-compatibility
   echo "Copying content of /tmp/.ssh to ~/.ssh"
-  mkdir ~/.ssh
+  mkdir -p ~/.ssh
   cp -r /tmp/.ssh/* ~/.ssh/
   chmod 600 ~/.ssh/*
   chmod 644 ~/.ssh/*.pub &> /dev/null
@@ -20,18 +26,19 @@ if [ -d /tmp/.ssh ]; then
 fi
 
 if [ ! -d /mnt/ssh ]; then
-  echo "No ssh directory found (~/.ssh, /tmp/.ssh, /mnt/ssh), exiting"
+  echo "No bind mounted ssh directory found (~/.ssh, /tmp/.ssh, /mnt/ssh), exiting"
   return 0
 fi
 
 if [ "$(stat -c '%U' /mnt/ssh)" != "UNKNOWN" ]; then
   echo "Unix host detected, symlinking /mnt/ssh to ~/.ssh"
+  rm -r ~/.ssh
   ln -s /mnt/ssh ~/.ssh
   return 0
 fi
 
 echo "Windows host detected, copying content of /mnt/ssh to ~/.ssh"
-mkdir ~/.ssh
+mkdir -p ~/.ssh
 cp -rf /mnt/ssh/* ~/.ssh/
 chmod 600 ~/.ssh/*
 chmod 644 ~/.ssh/*.pub &> /dev/null
