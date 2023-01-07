e00ff5fed3385652c15e86b17ad7e7454d2c84ed
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-01-07 11:19:49 +0100

    fix(ssh): return 0 instead of exit 0 - Avoid exiting chained commands - Avoid quitting shell

--- .windows.sh
+++ .windows.sh
@@ -6,7 +6,7 @@ if [ -d ~/.ssh ]; then
   # or it has been bind mounted with an older docker-compose.yml
   # for Linux and OSX, so we should leave it as is.
   echo "~/.ssh directory already exists, exiting"
-  exit 0
+  return 0
 fi
 
 if [ -d /tmp/.ssh ]; then
@@ -16,18 +16,18 @@ if [ -d /tmp/.ssh ]; then
   cp -r /tmp/.ssh/* ~/.ssh/
   chmod 600 ~/.ssh/*
   chmod 644 ~/.ssh/*.pub &> /dev/null
-  exit 0
+  return 0
 fi
 
 if [ ! -d /mnt/ssh ]; then
   echo "No ssh directory found (~/.ssh, /tmp/.ssh, /mnt/ssh), exiting"
-  exit 0
+  return 0
 fi
 
 if [ "$(stat -c '%U' /mnt/ssh)" != "UNKNOWN" ]; then
   echo "Unix host detected, symlinking /mnt/ssh to ~/.ssh"
   ln -s /mnt/ssh ~/.ssh
-  exit 0
+  return 0
 fi
 
 echo "Windows host detected, copying content of /mnt/ssh to ~/.ssh"
